f8f904ab5fd8857768159f7fe4b53dffcd1383f8
simptom000@gmail.com <goga166214>
2022-07-08 17:54:55 +0300

    Update README.md + comments at config.php

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
-Composer install
 
-config.php Stores all connection information
+
+Run in terminal composer install
+
+Change information in config.php for your account
+
+
+Your server need SSL certificate:
+
+if you use OpenServer for windows it's already have SSL 
+
+if you use Apache you should install it on your server
+
+or
+
+Save this certificate (https://curl.haxx.se/ca/cacert.pem) as cacert.pem in "Your_path"
+
+Add to php.ini:
+
+curl.cainfo = "Your_path\cacert.pem"
+
+openssl.cafile="Your_path\cacert.pem"
+
+You can run server by terminal 
+
+php -S localhost:8000
+
+
+
+
+Classes - folder with all classes 
+
+index.php - its main file
+
+post.php - for use form at main page. 
+
+index.phtml - html code 
+
+
 
 uploads folder contains all result files
 
--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 $credentials = [
-    'key' => 'AKIAQ4Q5FUQ6S5BHT3FY',
-    'secret' => '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',
+    'key' => 'AKIAQ4Q5FUQ634MGH2NA',
+    'secret' => 'EiM9+bbGaYbIymFyHTj+BOK5ANIRVQry7oEeOm32',
 ];
 
-$region = 'eu-north-1';
+$region = 'eu-north-1';// Your Amazon region for bucket and SQS
 
-$bucket = 'noxisestestbucket';
+$bucket = 'noxisestestbucket'; // Your Amazon S3 bucket name
 
-$folderForUploadFiles =''; //'test/'
+$folderForUploadFiles =''; //'test/' //Leave blank if the download will be without a folder
 
-$sqsQueueUrl = "https://sqs.eu-north-1.amazonaws.com/061264471101/test";
+$sqsQueueUrl = "https://sqs.eu-north-1.amazonaws.com/061264471101/test"; //Link to Amazon SQS message repository
 
-$fileNameFromS3 = '372212262620341.json';
+$fileNameFromS3 = '372212262620341.json'; // The filename that will be taken from the $bucket for parsing information
 
 $emailer = [
     'sender' => 'simptom000@gmail.com',
@@ -24,6 +24,6 @@ $emailer = [
         '<p>This email was sent with <a href="https://aws.amazon.com/ses/">' .
         'Amazon SES</a> using the <a href="https://aws.amazon.com/sdk-for-php/">' .
         'AWS SDK for PHP</a>.</p>'
-];
+]; //Information for emailSender
 
 
